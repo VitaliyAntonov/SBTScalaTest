43e43f2eac9592486d294f07f55c3125924f7a93
VitaliyAntonov <sinh.project@gmail.com>
2022-01-14 13:05:23 +0500

    пирамида тайлов для крупных масштабов

--- src/main/scala/folderIndex/FineScaleTails.scala
+++ src/main/scala/folderIndex/FineScaleTails.scala
@@ -17,19 +17,12 @@ import scala.collection.mutable.ArrayBuffer
  *  создаём папку 9, сливая по 4 рисунка и приводя их размер к 256 x 256 пикселей
  *  затем переходим на уровень вверх и из папки 9 создаём масштаб 8.
  *  Так продолжаем до 0-й папки. В 0-й папке создаём последний рисунок
- * @param path      - путь к исходной пирамиде тайлов
+ * @param pyramidPath      - путь к исходной пирамиде тайлов
  * @param upScale   - масштаб(номер папки) от которого строим пирамиду тайлов вверх
  */
-class FineScaleTails(path: String, upScale: Int) {
+class FineScaleTails(pyramidPath: String, upScale: Int) {
 
-  /** Текущий файл - левый верхний */
-  var numRow = 0          // Номер строки(имя папки)
-  var numColumn = 0       // Номер столбца(имя файла)
 
-  var upLeftPath = ""     // Путь к файлу верхний левый
-  var upRightPath = ""    // верхний правый
-  var downLeftPath = ""   // нижний левый
-  var downRightPath = ""  // нижний правый
 
   /** delimiter Разделитель в пути к файлу в зависимости от Операционной системы */
   val dm = {
@@ -39,24 +32,66 @@ class FineScaleTails(path: String, upScale: Int) {
 
   /** Путь к папке с масштабом upScale */
   val upScalePath: String = {
-    if (path.endsWith(dm)) path + upScale.toString
-    else path + dm + upScale.toString
+    if (pyramidPath.endsWith(dm)) pyramidPath + upScale.toString
+    else pyramidPath + dm + upScale.toString
   }
 
-  newPathCreate(pathNumberName(9))
+  /** Функция создаёт пирамиду тайлов крупных масштабов */
+  def mergeImagesAndSave = {
 
-  val list2 = inFoldersNames(path)
-  for(name <- list2._1) print(name + " "); println("")
-  for(name <- list2._2) print(name + " "); println("")
+    /** Текущий файл - левый верхний */
+    var numRow = 0          // Номер строки(имя папки)
+    var numColumn = 0       // Номер столбца(имя файла)
+
+    var activePath = ""     /** путь к обрабатываемой папке */
+
+    var upLeftPath = ""     // Путь к файлу верхний левый
+    var upRightPath = ""    // верхний правый
+    var downLeftPath = ""   // нижний левый
+    var downRightPath = ""  // нижний правый
+
+
+    /** Сканируем директорию исходной пирамиды */
+    val scanSource = inFoldersNames(pyramidPath)
+
+    /** сканируем папку с масштабом upScale */
+    val scanUpScale = inFoldersNames(upScalePath)
+
+    /** номер обрабатываемой папки(строки)  */
+    numRow = scanUpScale._1.minBy(x => x.toInt).toInt
+
+    /** путь к папке с минимальным номером, содержащей тайлы */
+    val minFolderPath = {
+      /** Ищем первую НЕ пустую папку */
+      var fExit = 0
+      val max = scanUpScale._1.maxBy(x => x.toInt).toInt
+      while(fExit == 0 && numRow <= max){
+        activePath = upScalePath + dm + numRow.toString
+        if(inFoldersNames(activePath)._2.nonEmpty) fExit = 1
+        else numRow += 1
+      }
+      activePath
+    }
+
+
+
+
+
+    // отладка
+    for (x <- scanSource._1) println(x) // скан исходно пирамиды
+    for (x <- scanUpScale._1) println(x) // скан исходной папки для построения пирамиды вверх
+    println(minFolderPath) // путь к папке с минимальным номером
+    // ------------------
+  }
 
 
   /**
-   * Функция возвращает путь, сформированный из path и номера папки
+   * Функция возвращает путь, сформированный из pyramidPath и номера папки
    * @param num  - номер папки, которую необходимо создать
    */
   def pathNumberName(num: Int): String ={
-    if (path.endsWith(dm)) path + num.toString
-    else path + dm + num.toString
+    if (pyramidPath.endsWith(dm)) pyramidPath + num.toString
+    else pyramidPath + dm + num.toString
   }
 
   /**
@@ -78,7 +113,7 @@ class FineScaleTails(path: String, upScale: Int) {
 
 
   /**
-   * Функция создаёт список имён папок, расположенных внутри указанной директории
+   * Функция создаёт список имён папок и файлов, расположенных внутри указанной директории
    * @param inPath   - директория для сканирования
    * @return      - Список имён папок
    */
@@ -97,8 +132,17 @@ class FineScaleTails(path: String, upScale: Int) {
         if(obj.isDirectory) pathsNames += obj.getName
       }
     }
+
     Tuple2(pathsNames.toArray.sortBy(w => w.toInt), filesNames.toArray.sortBy(w => w.slice(0,w.indexOf(".")).toInt ))
   }
+
+  /** Метод определяет пути к файлам рисунков, которые объединяются */
+  def setPathsImages = {
+
+  }
+
+
+
 
 }
 
@@ -112,21 +156,25 @@ object TestFineScaleTails extends App{
 //  C:\D\maps\rTails\m200\GoogleMapsCompatible\12
 // C:\D\maps\rTails\m200\GoogleMapsCompatible\12\1221
 
-  println(tFileScale.dm)
-  println(tFileScale.upScalePath)
+//  println(tFileScale.dm)
+//  println(tFileScale.upScalePath)
+//
+//  val list2 = tFileScale.inFoldersNames("C:\\D\\maps\\rTails\\m200\\GoogleMapsCompatible\\12\\1221")
+//  for(name <- list2._1) println(name + " "); println("")
+//  for(name <- list2._2){
+//    print(name.slice(0,name.indexOf(".")).toInt + " ")
+//    println(name)
+//  }
 
-  val list2 = tFileScale.inFoldersNames("C:\\D\\maps\\rTails\\m200\\GoogleMapsCompatible\\12\\1221")
-  for(name <- list2._1) println(name + " "); println("")
-  for(name <- list2._2){
-    print(name.slice(0,name.indexOf(".")).toInt + " ")
-    println(name)
-  }
+
+//  val words = "55 22 33 44 31 22".split(" ")
+//  val sorted = words.sortBy(w => w.length -> w.toLowerCase)
+//
+//  for(s <- sorted) print(s + " ")
 
 
-  val words = "55 22 33 44 31 22".split(" ")
-  val sorted = words.sortBy(w => w.length -> w.toLowerCase)
+  tFileScale.mergeImagesAndSave
 
-  for(s <- sorted) print(s + " ")
 }
 
 
